032db6dc9b6baf315f4572350d67420368ae80d2
hemjay07 <mujeebopabode07@gmail.com>
2024-11-04 05:33:50 -0800

    style: style

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-300">
       <header className="bg-white shadow-md">
-        <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-2xl font-bold text-gray-900">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">
             Crypto Tracker
           </h1>
         </div>
       </header>
       <main className="p-4">
-        <div className="max-w-7xl mx-auto py-2 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <CryptoTable />
         </div>
       </main>
